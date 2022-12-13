b3ce6c8f7cb24279f82ae14e9a0ab726ab0277c9
eric6311 <hbea6311@gmail.com>
2022-12-13 20:45:25 +0800

    add etherscan verifying

--- contracts/CryptoDevToken.sol
+++ contracts/CryptoDevToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts@4.8.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CryptoDevToken is ERC20 {
     constructor() ERC20("CryptoDevToken", "CD") {
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,13 @@
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const PRIVATEKEY = process.env.PRIVATE_KEY;
 const endPoint = process.env.RPC_URL;
+const etherscan_key = process.env.ETHERSCAN_API;
+
 module.exports = {
   defaultNetworks: "hardhat",
   networks: {
@@ -15,6 +18,10 @@ module.exports = {
       accounts: [PRIVATEKEY],
       chainId: 5,
     },
+  },
+
+  etherscan: {
+    apiKey: etherscan_key,
   },
 
   solidity: {
